4b33ee635086a47f6e765ae952cf4b3e7fe5629c
Erik Holden <erikholden4@gmail.com>
2023-12-20 11:21:49 -0500

    initial push

---
+++ src/test/java/com/pluralsight/NorthwindTradersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
